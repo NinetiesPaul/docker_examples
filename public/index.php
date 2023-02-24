eb5a45c089952e85e0a4b0ce4215cdcd7e40d72b
Paul Richard <paulrpm@gmail.com>
2023-02-24 13:02:11 -0300

    updated index

--- public/index.php
+++ public/index.php
@@ -1,3 +1,5 @@
 <?php
 
-echo "This is a PHP app running on Docker";
+echo "<p>This is a PHP base app running on Docker</p>";
+
+echo "<p> Run <i>docker-compose exec composer</i> to install a framework or any library and start coding!";
